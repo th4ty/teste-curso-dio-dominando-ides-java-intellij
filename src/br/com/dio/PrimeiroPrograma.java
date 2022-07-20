1e30b4204bdf3a4d2d726709c4d31d6bbae20f82
th4ty <thatiane.ribeiro94@gmail.com>
2022-07-20 01:25:42 -0300

    correcao na class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,7 +7,7 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-        Livro livro1 = new Livro(nome;: "O problema dos 3 corpos", numPaginas:300);
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
         System.out.println(livro1);
 
         /*int a = 5;
